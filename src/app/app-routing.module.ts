3ff5ba501d382a960d574c6bc9cdc1177f8a11ab
Sateesh Perumalla <sateeshperumalla28@gmail.com>
2023-05-12 05:24:26 -0700

    Updated Angular files and remove comments in App Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { ItemsListComponent } from './items/items-list/items-list.component';
+import { ProductDetailsComponent } from './product-item/product-details/product-details.component';
 
 const routes: Routes = [
   { path: 'home', component: ItemsListComponent },
   { path: 'cart', component: CartComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'products/:id', component: ProductDetailsComponent }
 ];
 
 @NgModule({
