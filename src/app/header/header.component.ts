7f1b79babd380ad5a0f2271663e90da7e8f57fdd
Sateesh Perumalla <sateeshperumalla28@gmail.com>
2023-05-11 19:18:20 -0700

    Added header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/models/app-state.model';
+import { CartItem } from 'src/models/cart-item.model';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  cartItems: Observable<CartItem[]>;
+  cartItemsCount = 0;
+  constructor(private store: Store<AppState>, private router: Router) {
+  }
+  ngOnInit(): void {
+    this.cartItems = this.store.select(store => store.shopping.cart);
+    this.cartItems.subscribe((data) => {
+      let count = 0;
+      data.forEach(item => {
+        count = count + item.quantity;
+      })
+      this.cartItemsCount = count;
+    })
+  }
+  onNavigateCart() {
+    this.router.navigate(['/cart'])
+  }
+  onNavigateHome() {
+    this.router.navigate(['/'])
+  }
+}
